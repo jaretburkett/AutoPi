7dbe7da52b2682d139bcef6291a52868385013dc
Jaret Burkett <jaretburkett@gmail.com>
2016-11-02 13:47:55 -0500

    Made a remote installer

---
+++ README.md
@@ -0,0 +1,6 @@
+## Auto Pi
+#Under Construction
+
+To install run the following command
+
+`sudo curl -sL https://raw.githubusercontent.com/jaretburkett/AutoPi/master/remoteinstall | sudo bash -`
--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ ln -s /tmp/random-seed /var/lib/systemd/random-seed
 sed -i '/RemainAfterExit=yes/a ExecStartPre=/bin/echo "" >\/tmp\/random-seed' /lib/systemd/system/systemd-random-seed.service
 
 #disable filesystem check and swap
-sed -i 's/rootfstype=ext4/rootfstype=ext4 fastboot noswap quiet rw/g' /boot/cmdline.txt
+sed -i 's/console=tty1/console=tty1 loglevel=3 logo.nologo fastboot noswap quiet rw/g' /boot/cmdline.txt
 
 echo 'tmpfs           /tmp            tmpfs   nosuid,nodev         0       0' >> /etc/fstab
 echo 'tmpfs           /var/log        tmpfs   nosuid,nodev         0       0' >> /etc/fstab
@@ -46,17 +46,17 @@ echo '# setup fancy prompt' >> /etc/bash.bashrc
 echo 'PROMPT_COMMAND=set_bash_prompt' >> /etc/bash.bashrc
 
 #install chromium
-#wget http://launchpadlibrarian.net/201290259/libgcrypt11_1.5.3-2ubuntu4.2_armhf.deb
-#wget http://launchpadlibrarian.net/219267135/chromium-codecs-ffmpeg-extra_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
-#wget http://launchpadlibrarian.net/219267133/chromium-browser_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
+wget http://launchpadlibrarian.net/201290259/libgcrypt11_1.5.3-2ubuntu4.2_armhf.deb
+wget http://launchpadlibrarian.net/219267135/chromium-codecs-ffmpeg-extra_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
+wget http://launchpadlibrarian.net/219267133/chromium-browser_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
 
-#sudo dpkg -i libgcrypt11_1.5.3-2ubuntu4.2_armhf.deb
-#sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
-#sudo dpkg -i chromium-browser_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
+sudo dpkg -i libgcrypt11_1.5.3-2ubuntu4.2_armhf.deb
+sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
+sudo dpkg -i chromium-browser_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
 
-#sudo rm libgcrypt11_1.5.3-2ubuntu4.2_armhf.deb
-#sudo rm chromium-codecs-ffmpeg-extra_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
-#sudo rm chromium-browser_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
+sudo rm libgcrypt11_1.5.3-2ubuntu4.2_armhf.deb
+sudo rm chromium-codecs-ffmpeg-extra_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
+sudo rm chromium-browser_45.0.2454.101-0ubuntu0.14.04.1.1099_armhf.deb
 
 #install packages
 sudo apt-get remove nodered -y
@@ -68,6 +68,9 @@ sudo apt-get install -y nodejs
 sudo apt-get install unclutter -y
 sudo apt-get install fbi -y
 sudo apt-get -f install -y
+sudo apt-get remove -y --purge scratch squeak-plugins-scratch squeak-vm wolfram-engine python-minecraftpi minecraft-pi sonic-pi oracle-java8-jdk bluej greenfoot libreoffice-common libreoffice-core freepats
+sudo apt-get autoremove -y
+sudo apt-get install -y --force-yes xdotool matchbox-window-manager xorg lightdm xinit
 sudo npm install
 
 #set chromium on boot
@@ -77,22 +80,35 @@ sudo npm install
 #echo '@xset -dpms' >> /home/pi/.config/lxsession/LXDE-pi/autostart
 #echo '@chromium-browser --noerrdialogs --kiosk --incognito http://localhost:8080' >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
+#tell pi to boot into gui and turn on componets
+echo "start_x=1" >> /boot/config.txt
+echo "dtparam=spi=on" >> /boot/config.txt
+
+#hide power warning
+echo "avoid_warnings=1" >> /boot/config.txt
+
+#boot using matchbox
+echo '#!/bin/bash' >> /home/pi/.xsession
+echo 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:8080 &' >> /home/pi/.xsession
+echo 'exec matchbox-window-manager  -use_titlebar no' >> /home/pi/.xsession
+
+#prevent screen off
+echo "BLANK_TIME=0" >> /etc/kbd/config
+echo "POWERDOWN_TIME=0" >> /etc/kbd/config
+
+
 # autostart node
-echo '#!/bin/sh' >> /etc/init.d/startautopi
-echo '/usr/bin/node /AutoPi/index.js' >> /etc/init.d/startautopi
-sudo chmod 755 /etc/init.d/startautopi
-sudo update-rc.d startautopi defaults
+sudo cp /AutoPi/pifiles/startautopi /etc/init.d/startautopi
+sudo chmod a+x /etc/init.d/startautopi
+sudo insserv /etc/init.d/startautopi
 
 #setup splash screen
-sudo cp /AutoPi/pifiles/autopisplash /etc/init.d/autopisplash
-sudo chmod a+x /etc/init.d/autopisplash
-sudo insserv /etc/init.d/autopisplash
+sudo cp /AutoPi/pifiles/autopisplash /etc/init.d/aaautopisplash
+sudo chmod a+x /etc/init.d/aaautopisplash
+sudo insserv /etc/init.d/aaautopisplash
 #clear text from screen so we dont see it on boot
 sed -i '/fi/a clear' /etc/rc.local
 
-#change boot to program
-rm /boot/fullpageos.txt
-echo 'http://localhost:8080' >> /boot/fullpageos.txt
 
 
 echo ""
