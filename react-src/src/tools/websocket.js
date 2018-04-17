420eb4cd242b599e68d6afc006d53534ffd172f3
Jaret Burkett <jaretburkett@gmail.com>
2018-04-17 08:29:28 -0500

    Reload page for soft update

--- react-src/src/tools/websocket.js
+++ react-src/src/tools/websocket.js
@@ -3,16 +3,22 @@ const socketURL = process.env.NODE_ENV === 'development'? 'http://localhost:8080
 const socket = openSocket(socketURL);
 
 export function startWebsocket(store){
+    let needToReload = false;
     // assign to the window object for development
     window.socket = socket;
     socket.on('connect', () => {
         console.log(`Connected to ${socketURL}`);
         store.connected = true;
+        // on a reconnect, soft update probably occurred, reload
+        if(needToReload){
+            location.reload();
+        }
     });
 
     socket.on('disconnect', () => {
         console.log('Disconnected from websocket');
         store.connected = false;
+        needToReload = true;
     });
 
     socket.on('store', (data) => {
