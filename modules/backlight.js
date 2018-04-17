d060d727d31b8d0fdfcc7aac94b7ea372f5b4173
Jaret Burkett <jaretburkett@gmail.com>
2018-04-17 09:28:23 -0500

    Min brightness for pi display set to 10

--- modules/backlight.js
+++ modules/backlight.js
@@ -6,7 +6,7 @@ function setBrightness(percent){
     if(tools.isPi()){
         // value of 0 - 255. Don't lower below 10
         // map percent to byte value.
-        const backlightValue = tools.map(percent,0,100,20,255).toFixed(0);
+        const backlightValue = tools.map(percent,0,100,10,255).toFixed(0);
         exec(`echo ${backlightValue} > /sys/class/backlight/rpi_backlight/brightness`, (err, stdout, stderr) => {
             // ignore
         });
